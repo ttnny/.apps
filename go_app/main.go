7ee015c9f8eeba8a8e72596cd3a0d91a46c2ceeb
ttnny <ttnny@users.noreply.github.com>
2019-12-16 03:07:51 -0800

    Add index page for app

--- go_app/main.go
+++ go_app/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
 var router = mux.NewRouter()
+var tmplPartials = "go_app/templates/_partials.gohtml"
 
 // Register the mux router to serve requests in App Engine
 func init() {
@@ -16,13 +17,16 @@ func init() {
 }
 
 func main() {
-	router.HandleFunc("/", indexHandler)
+	router.HandleFunc("/", indexHandler).Methods("GET")
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
+
+	// Serve static files
+	router.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("go_app/assets"))))
 
 	log.Printf("Listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
@@ -32,9 +36,8 @@ func main() {
 
 // indexHandler for apps homepage
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
+	tmpl := template.Must(template.ParseFiles(tmplPartials, "go_app/templates/index.gohtml"))
+	if err := tmpl.ExecuteTemplate(w, "index.gohtml", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
-	fmt.Fprint(w, "This is a very first deploy of the apps page to App Engine.")
 }
